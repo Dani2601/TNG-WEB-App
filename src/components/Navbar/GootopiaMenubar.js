54c298f5bbc729d8d92e67de48cbbe2d4f2c387d
Deinen Ryio Flores <deinenf@gmail.com>
2023-03-29 07:04:19 +0800

    Gootopia Navbar Web

--- src/components/Navbar/GootopiaMenubar.js
+++ src/components/Navbar/GootopiaMenubar.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { MdClose, MdMenu } from "react-icons/md";
 import { desertlogo, tdmnav } from "../../assets";
-import close from "../../assets/Gootopia/closebutton.png"
-import hamburger from "../../assets/Gootopia/hamburgericon.png"
-import gootopia from "../../assets/Gootopia/gootopiamobile.png"
+import close from "../../assets/Gootopia/closebutton.png";
+import hamburger from "../../assets/Gootopia/hamburgericon.png";
+import gootopia from "../../assets/Gootopia/gootopiamobile.png";
+import dripping from "../../assets/Gootopia/slimedripping.png";
+import routes from "../../constants/routes";
 
 export default function DesertMuseumMenubar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,115 +15,119 @@ export default function DesertMuseumMenubar() {
   }
 
   return (
-    <nav className="bg-gootopia-green font-flavors">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between">
-          <div className="flex w-full justify-start items-center">
-            <div className="hidden w-1/2 md:block">
-              <div className="vg flex items-baseline">
-                <a
-                  href="#"
-                  className="w-1/3 text-center text-white rounded-md text-sm font-medium"
-                >
-                  Obstacles
-                </a>
-                <a
-                  href="#"
-                  className="w-1/3 text-center text-white rounded-md text-sm font-medium"
-                >
-                  Contacts
-                </a>
-                <a
-                  href="#"
-                  className="w-1/3 text-center text-white rounded-md text-sm font-medium"
-                >
-                  FAQs
-                </a>
+    <>
+      <nav className="bg-gootopia-green font-flavors">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between">
+            <div className="flex w-full justify-start items-center">
+              {!isOpen && (
+                <div className="flex-shrink-0">
+                  <img
+                    className=" object-contain"
+                    src={gootopia}
+                    alt="Workflow"
+                  />
+                </div>
+              )}
+              <div className="hidden w-1/2 md:block">
+                <div className="vg flex items-baseline">
+                  <a
+                    href={routes.LandingGootopia}
+                    className="w-1/3 text-center text-black rounded-md text-sm font-medium"
+                  >
+                    Home
+                  </a>
+                  <a
+                    href="#"
+                    className="w-1/3 text-center text-black rounded-md text-sm font-medium"
+                  >
+                    Obstacles
+                  </a>
+                  <a
+                    href="#"
+                    className="w-1/3 text-center text-black rounded-md text-sm font-medium"
+                  >
+                    Contacts
+                  </a>
+                  <a
+                    href="#"
+                    className="w-1/3 text-center text-black rounded-md text-sm font-medium"
+                  >
+                    FAQs
+                  </a>
+                </div>
+              </div>
+
+              <div className="w-1/2 hidden md:block">
+                <div className="vg flex items-baseline space-x-4">
+                  <a
+                    href="#"
+                    className="w-1/3 text-center text-black rounded-md text-sm font-medium"
+                  >
+                    HOW TO BOOK
+                  </a>
+                  <a
+                    href="#"
+                    className="w-1/3 text-center text-black rounded-md text-sm font-medium"
+                  >
+                    FAQS
+                  </a>
+                  <a
+                    href="#"
+                    className="w-1/3 text-center text-black rounded-md text-sm font-medium"
+                  >
+                    LOGIN
+                  </a>
+                </div>
               </div>
             </div>
-            {!isOpen && (
-              <div className="flex-shrink-0">
-                <img
-                  className=" object-contain"
-                  src={gootopia}
-                  alt="Workflow"
-                />
-              </div>
-            )}
-            <div className="w-1/2 hidden md:block">
-              <div className="vg flex items-baseline space-x-4">
-                <a
-                  href="#"
-                  className="w-1/3 text-center text-white rounded-md text-sm font-medium"
-                >
-                  HOW TO BOOK
-                </a>
-                <a
-                  href="#"
-                  className="w-1/3 text-center text-white rounded-md text-sm font-medium"
-                >
-                  FAQS
-                </a>
-                <a
-                  href="#"
-                  className="w-1/3 text-center text-white rounded-md text-sm font-medium"
-                >
-                  LOGIN
-                </a>
-              </div>
+            <div className="-mr-2 flex md:hidden">
+              <button onClick={toggleMenu} type="button" aria-expanded="false">
+                {!isOpen ? (
+                  <img className="" src={hamburger} alt="Workflow" />
+                ) : (
+                  <img className="mt-5" src={close} alt="Workflow" />
+                )}
+              </button>
             </div>
           </div>
-          <div className="-mr-2 flex md:hidden">
-            <button onClick={toggleMenu} type="button" aria-expanded="false">
-              {!isOpen ? (
-                <img
-                className=""
-                src={hamburger}
-                alt="Workflow"
-              />              ) : (
-                <img
-                className="mt-5"
-                src={close}
-                alt="Workflow"
-              />              )}
-            </button>
+        </div>
+        <div className={`${isOpen ? "" : "hidden"} md:hidden w-full h-screen`}>
+          <div className="flex flex-col items-center px-2 pt-2 pb-3 space-y-1 sm:px-3 ">
+            <a
+              href={routes.LandingGootopia}
+              className="  block px-3 py-2 w-full text-left text-base"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="  block px-3 py-2 w-full text-left text-base"
+            >
+              Obstacles
+            </a>
+            <a
+              href="#"
+              className="  block px-3 py-2 w-full text-left text-base"
+            >
+              FAQs
+            </a>
+            <a
+              href="#"
+              className="  block px-3 py-2 w-full text-left text-base"
+            >
+              Packages
+            </a>
+            <a
+              href="#"
+              className="  block px-3 py-2 w-full text-left text-base"
+            >
+              Book Now
+            </a>
           </div>
         </div>
-      </div>
-      <div className={`${isOpen ? "" : "hidden"} md:hidden w-full h-screen`}>
-        <div className="flex flex-col items-center px-2 pt-2 pb-3 space-y-1 sm:px-3 ">
-          <a
-            href="#"
-            className="  block px-3 py-2 w-full text-left text-base"
-          >
-            Home
-          </a>
-          <a
-            href="#"
-            className="  block px-3 py-2 w-full text-left text-base"
-          >
-            Obstacles
-          </a>
-          <a
-            href="#"
-            className="  block px-3 py-2 w-full text-left text-base"
-          >
-            FAQs
-          </a>
-          <a
-            href="#"
-            className="  block px-3 py-2 w-full text-left text-base"
-          >
-            Packages
-          </a>
-          <a
-            href="#"
-            className="  block px-3 py-2 w-full text-left text-base"
-          >
-            Book Now
-          </a>
-        </div>
-      </div>
-    </nav>
+      </nav>
+      {isOpen ? <></> : <img className=" " src={dripping} alt="dripping" />}
+    </>
   );
 }
