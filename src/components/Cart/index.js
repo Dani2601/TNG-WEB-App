49f038e5a090b98bcc2cf0118c5916417ae7cc8e
Joseph Clemente <joseph.clemente@hacktiv.io>
2023-07-05 23:06:09 +0800

    fix route

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -5,17 +5,34 @@ import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import routes from '../../constants/routes'
 
+const DESSERT_KEY = process.env.REACT_APP_DESSERT_KEY;
+const GOOTOPIA_KEY = process.env.REACT_APP_GOOTOPIA_KEY;
+const TFR_KEY = process.env.REACT_APP_TFR_KEY;
+const TIS_KEY = process.env.REACT_APP_INFLATABLE_KEY;
+const BAKEBE_KEY = process.env.REACT_APP_BAKEBE_KEY;
+
+const business_unit = {
+    [BAKEBE_KEY]: routes.BookingBakebe,
+    [GOOTOPIA_KEY]: routes.BookingGootopia,
+    [TFR_KEY]: routes.BookingTFR,
+    [DESSERT_KEY]: routes.DessertBooking,
+    [TIS_KEY]: routes.BookingInflatable
+}
+
 export default function Cart() {
     
     const { cart } = useSelector(state => state.record);
     const navigate = useNavigate()
 
     const handleCartClick = () => {
-        navigate(routes.DessertBooking, {state: {step: true}})
+        if(cart.length > 0){
+            const checkBusiness = cart[0]?.BusinessUnitID
+            navigate(business_unit[checkBusiness], {state: {step: true}})
+        }
     };
     
     return (
-        <div className="cart cursor-pointer" onClick={handleCartClick}>
+        <div className={`cart ${cart.length > 0 && 'cursor-pointer'}`} onClick={handleCartClick}>
             {
                 cart?.length > 0 &&
                 (
