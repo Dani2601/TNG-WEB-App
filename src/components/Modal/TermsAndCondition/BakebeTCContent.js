b2b0b6577a9052bb06f8a3bdae8248abe1522c2c
Deinen Ryio Flores <deinenf@gmail.com>
2023-09-16 16:25:05 +0800

    new T&C

--- src/components/Modal/TermsAndCondition/BakebeTCContent.js
+++ src/components/Modal/TermsAndCondition/BakebeTCContent.js
@@ -1,55 +1,115 @@
-import React from 'react'
+import React from "react";
 
 export default function BakebeTCContent() {
   return (
     <div className="flex-grow overflow-y-auto">
-    <ul className="list-disc list-inside flex flex-col gap-4">
-            <li className='text-sm text-justify'>A peer charge of Php 500 per person will be 
-            included when baking one product with a companion 
-            (maximum of 2 people per each project). 
-            </li>
-            <li className='text-sm text-justify'>Children under 12 must always have 1 on 1 
-            parental supervision to attend Bakebe's class. A 
-            guardian waiver form must be signed before starting 
-            our baking project. The youngest kid we can 
-            accommodate is only 6-8 years old, but will depend on 
-            their behavior please note that this is still a kitchen 
-            where safety is a priority above all.
-            </li>
-            <li className='text-sm text-justify'>Guests are required to come at least 15 
-            MINUTES BEFORE THEIR SCHEDULED TIME 
-            SLOT FOR THE PROCESSING OF TICKETS.
-            </li>
-            <li className='text-sm text-justify'>
-            No pets allowed. 7. No refunds, but 
-            rebooking with a Php 300 charge is allowed within 3 
-            days of the booked date.
-            </li>
-            <li className='text-sm text-justify'>LATE GOERS are subjected to a 
-            rescheduling fee of PHP200/head and can only be 
-            accommodated upon slot availability. 
-            </li>
-            <li className='text-sm text-justify'>
-                NO SHOWS can be rescheduled up to three 
-                (3) days after the guest/s booked date since a LATE 
-                RESCHEDULING FEE CAN ONLY BE DONE 
-                ONCE and that will also be subjected to a PHP 300 
-                rescheduling fee for slot availability. After more than 
-                seven (7) days of your booked date, attempts to 
-                reschedule will not be allowed and the guest/s slots can 
-                or will be forfeited depending on the situation.
-            </li>
-            <li className='text-sm text-justify'>Tickets will not be refundable but may be transferable provided that it has the authorization letter and a valid 
-                ID of the owner and the one who will use it.
-            </li>
-            <li className='text-sm text-justify'>Bakebe Philippines is not responsible for any personal items inside or outside the premises.
-            </li>
-            <li className='text-sm text-justify'>Guest/s will be held responsible for any damage caused due to negligence.
-            </li>
-            <li className='text-sm text-justify'>Management will not be held responsible for injuries sustained on the premises if the guest/s did not follow the rules and regulations.</li>
-            <li className='text-sm text-justify'>Management reserves the right to amend the terms & conditions without prior notice.</li>
-            <li className='text-sm text-justify'>Management reserves the right to refuse entry to people who do not follow the house policies.</li>
-        </ul>
+      <ul className="list-disc list-inside flex flex-col gap-4">
+        <li className="text-sm text-justify">
+          A peer charge of Php 500 per person will be included when baking one
+          product with a companion (maximum of 2 people per each project).
+        </li>
+        <li className="text-sm text-justify">
+          Children under 12 years old must always have 1 on 1 parental
+          supervision to attend Bakebe’s class. Guardian waiver form must be
+          signed before starting our baking project. Youngest kid we can
+          accommodate is only 6-8 years old, but it will depend on their
+          behavior as please note that this is still a kitchen where safety is a
+          priority above all.
+        </li>
+        <li className="text-sm text-justify">
+          Each project price is included: Recipes, pre-set ingredients, and
+          decorations for 1 person. Dirty dishes cleaning service, cake experts’
+          assistance, and a cake box with an elegant ribbon for you to bring it
+          away as a gift.
+        </li>
+        <li className="text-sm text-justify">
+          Guests are required to come at least 15 MINUTES BEFORE THEIR SCHEDULED
+          TIME SLOT FOR THE PROCESSING OF TICKETS.
+        </li>
+        <li className="text-sm text-justify">
+          Entries after 6:30pm can only choose to bake products with baking time
+          of 2.5 hours (Store closing time is 10pm; subject to change, depending
+          on Mall operating hours)
+        </li>
+        <li className="text-sm text-justify">No pets allowed.</li>
+        <li className="text-sm text-justify">
+          Operating hours are from 10AM to 10PM, the first wave to bake is at
+          10AM and our last wave is at 6PM for cake and 6:30PM for small
+          pastries. Operating hours might change depending on mall hours
+          especially on declared holidays in the Philippines.
+        </li>
+        <li className="text-sm text-justify">
+          Booking confirmation will be sent via email.
+        </li>
+        <li className="text-sm text-justify">
+          All guests must sign a waiver form/guest detail form upon entering the
+          vicinity.
+        </li>
+        <li className="text-sm text-justify">
+          LATE GOERS are subjected to a rescheduling fee of PHP200/head and can
+          only be accommodated upon slot availability.
+        </li>
+        <li className="text-sm text-justify">
+          ALL TICKETS/ RESERVATIONS CAN BE RESCHEDULED 3 DAYS PRIOR OF VISIT AND
+          THE NEW SCHEDULE SHOULD BE WITHIN THE MONTH OF RESERVATION ONLY. THIS
+          IS SUBJECT TO A rescheduling FEE OF PHP300 PER PROJECT.
+        </li>
+        <li className="text-sm text-justify">
+          Tickets are NON-REFUNDABLE BUT MAY BE TRANSFERRED.
+        </li>
+        <li className="text-sm text-justify">
+          INCLEMENT WEATHER POLICY – In the event of severe weather conditions,
+          no refunds will be given but Bakebe Philippines will provide FREE
+          RESCHEDULING to all patrons within one (1) month of the booked date.
+        </li>
+        <li className="text-sm text-justify">
+          Some parts of the area may also be up for maintenance which would not
+          be grounds for refunds.
+        </li>
+        <li className="text-sm text-justify">
+          Guests will be held responsible for any damage caused due to
+          negligence.
+        </li>
+        <li className="text-sm text-justify">
+          Bakebe Philippines is not responsible for any personal items inside or
+          outside the premises.
+        </li>
+        <li className="text-sm text-justify">
+          Tickets will not be refundable but may be transferable if it has the
+          authorization letter and a valid ID of the owner and the one who will
+          use it.
+        </li>
+        <li className="text-sm text-justify">
+          Management will not be held responsible for injuries sustained in the
+          premises if the guest/s did not follow the rules and regulations.
+        </li>
+        <li className="text-sm text-justify">
+          Management reserves the right to refuse entry of people who do not
+          follow the house policies.
+        </li>
+        <li className="text-sm text-justify">
+          Management reserves the right to change prices without prior notice.
+        </li>
+        <li className="text-sm text-justify">
+          Management may photograph, film, videotape, record or otherwise
+          reproduce the image and/or voice of any person who uses the hashtag
+          #BAKEBEPH #BAKEBE or tag our official social media fan pages and use
+          the same or any purpose without permission or payment.
+        </li>
+        <li className="text-sm text-justify">
+          Management reserves the right to amend the terms & conditions without
+          prior notice.
+        </li>
+        <li className="text-sm text-justify">
+          PROMOTIONAL TICKETS – Tickets that are on sale cannot be combined with
+          any other existing promotions, online deals and government mandated
+          discounts. Specific promotional mechanics, terms and conditions shall
+          apply.
+        </li>
+        <li className="text-sm text-justify">
+          Smoking is strictly not allowed.
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
