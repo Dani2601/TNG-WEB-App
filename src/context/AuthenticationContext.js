ee1a98bb8832a535e7e199f4338f42071efae2e9
Joseph <joseph.clemente@hacktiv.io>
2024-01-03 17:27:42 +0800

    reset cart

--- src/context/AuthenticationContext.js
+++ src/context/AuthenticationContext.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { createContext, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
-import { setToken, setUser } from '../store/action';
+import { setCart, setToken, setUser } from '../store/action';
 import { reauthenticate } from '../functions';
 
 const AuthContext = createContext({});
@@ -16,35 +16,38 @@ const AuthProvider = (props) => {
 
   useEffect(() => {
       let userToken = token;
-      //console.log(userToken)
       if (userToken != null) {
         setLoading(true)
         reauthenticate(userToken)
         .then((res) => {
           if(res.valid){
-            dispatch(setUser(res.user))
-            dispatch(setToken(res.token))
+            dispatch(setUser(res.user));
+            dispatch(setToken(res.token));
+            dispatch(setCart([]));
             setLoggedIn(true);
-            setLoading(false)
+            setLoading(false);
           }
           else{
             dispatch(setUser(null))
             dispatch(setToken(null))
+            dispatch(setCart([]));
             setLoggedIn(false);
             setLoading(false)
           }
         })
         .catch((e) => {
-          dispatch(setUser(null))
-          dispatch(setToken(null))
+          dispatch(setUser(null));
+          dispatch(setToken(null));
+          dispatch(setCart([]));
           setLoggedIn(false);
-          setLoading(false)
+          setLoading(false);
         })
       } else {
-        dispatch(setUser(null))
-        dispatch(setToken(null))
+        dispatch(setUser(null));
+        dispatch(setToken(null));
+        dispatch(setCart([]));
         setLoggedIn(false);
-        setLoading(false)
+        setLoading(false);
       }
   }, []);
   
